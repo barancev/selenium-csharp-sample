a7d907ea4235710e4d0a797e141f3516a3fd7458
Alexei Barantsev <barancev@gmail.com>
2014-11-03 17:31:19 +0300

    Separating initialization and finalization code from the tests

--- selenium-csharp-sample-solution/selenium-csharp-sample-project/UnitTest1.cs
+++ selenium-csharp-sample-solution/selenium-csharp-sample-project/UnitTest1.cs
@@ -10,26 +10,36 @@ namespace php4dvdtests
     [TestFixture()]
     public class Php4DvdTests
     {
+        private IWebDriver wd;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            wd = new FirefoxDriver();
+            wd.Navigate().GoToUrl("http://localhost/php4dvd/");
+        }
+
         [Test()]
         public void LoginTest()
         {
-            IWebDriver wd = new FirefoxDriver();
-            try
-            {
-                wd.Navigate().GoToUrl("http://192.168.0.100/php4dvd/");
-                wd.FindElement(By.Id("username")).Click();
-                wd.FindElement(By.Id("username")).Clear();
-                wd.FindElement(By.Id("username")).SendKeys("admin");
-                wd.FindElement(By.Name("password")).Click();
-                wd.FindElement(By.Name("password")).Clear();
-                wd.FindElement(By.Name("password")).SendKeys("admin");
-                wd.FindElement(By.Name("submit")).Click();
-                wd.FindElement(By.LinkText("Log out")).Click();
-                wd.SwitchTo().Alert().Accept();
-            }
-            finally
+            wd.FindElement(By.Id("username")).Click();
+            wd.FindElement(By.Id("username")).Clear();
+            wd.FindElement(By.Id("username")).SendKeys("admin");
+            wd.FindElement(By.Name("password")).Click();
+            wd.FindElement(By.Name("password")).Clear();
+            wd.FindElement(By.Name("password")).SendKeys("admin");
+            wd.FindElement(By.Name("submit")).Click();
+            wd.FindElement(By.LinkText("Log out")).Click();
+            wd.SwitchTo().Alert().Accept();
+        }
+
+        [TearDown]
+        public void StopBrowser()
+        {
+            if (wd != null)
             {
                 wd.Quit();
+                wd = null;
             }
         }
     }

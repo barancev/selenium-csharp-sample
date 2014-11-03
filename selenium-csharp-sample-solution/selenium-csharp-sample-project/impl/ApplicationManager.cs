0e3b9689a9a77562ae803693bb7eb01ddbe180d0
Alexei Barantsev <barancev@gmail.com>
2014-11-04 00:10:07 +0300

    Preparing structure of "implementation layer"

---
+++ selenium-csharp-sample-solution/selenium-csharp-sample-project/impl/ApplicationManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace php4dvdtests
+{
+    public class ApplicationManager
+    {
+        public ApplicationManager(ICapabilities capabilities, string baseUrl, string hubUrl)
+        {
+            Driver = WebDriverFactory.GetDriver(hubUrl, capabilities);
+            BaseURL = baseUrl;
+
+            Auth = new LoginHelper(this);
+            Navigator = new NavigationHelper(this);
+        }
+
+        public IWebDriver Driver { get; set; }
+
+        public string BaseURL { get; set; }
+
+        public LoginHelper Auth { get; set; }
+
+        public NavigationHelper Navigator { get; set; }
+    }
+}
---
+++ selenium-csharp-sample-solution/selenium-csharp-sample-project/impl/NavigationHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace php4dvdtests
+{
+    public class NavigationHelper
+    {
+        private ApplicationManager manager;
+
+        public NavigationHelper(ApplicationManager manager)
+        {
+            this.manager = manager;
+        }
+    }
+}
